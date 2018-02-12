81c96a5b5a45be5ba15b0ecd98999349d4156b5c
Lia <blue.star95@outlook.com>
2018-02-13 00:20:37 +0700

    what

--- examples/smote_test.py
+++ examples/smote_test.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 import sys
 
-input_file = '/home/lia/Documents/the_project/dataset/iris.csv'
+input_file = '/home/lia/Documents/the_project/dataset/iris_imb.csv'
 df = pd.read_csv(input_file)
 
 print(len(df))
@@ -21,12 +21,9 @@ X_sm, y_sm = smote.fit_sample(X, y.values.ravel())
 X_sm = pd.DataFrame(X_sm, index=None,columns=['sepallength','sepalwidth','petallength','petalwidth'])
 y_sm = pd.DataFrame(y_sm, index=None, columns=['class'])
 
-# print(X_sm)
-# sys.exit()
-
-#
-# print(len(X_sm))
-# print(X_sm)
+print(X)
+print(len(X_sm))
+print(X_sm)
 
 pca = PCA(n_components=2)
 
@@ -51,15 +48,6 @@ for target, color in zip(targets,colors):
                , s = 50)
 ax.legend(targets)
 ax.grid()
-
-# Initialize the plot
-# fig = plt.figure(figsize=(20,10))
-# ax1 = fig.add_subplot(121)
-# ax2 = fig.add_subplot(122)
-
-# Plot the data
-# ax1.scatter(X_vis[y == 0, 0], X_vis[y == 0, 1], alpha=0.5)
-# ax2.bar([0,1,2],[0.5,1,2.5])
 
 # Show the plot
 plt.show()
