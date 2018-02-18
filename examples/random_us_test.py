c17ca3dcb7622707c03697e452665d52c5b5891f
Lia <blue.star95@outlook.com>
2018-02-18 23:01:18 +0700

    random undersampling is working

--- examples/random_us_test.py
+++ examples/random_us_test.py
@@ -1,13 +1,14 @@
 from collections import Counter
 
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
 
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.pipeline import make_pipeline as make_pipeline_imb
 from imblearn.metrics import classification_report_imbalanced
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 from sklearn.model_selection import cross_val_score, cross_val_predict, train_test_split
 
@@ -24,13 +25,16 @@ df = pd.read_csv(input_file)
 X_data = df['reviewText']
 y_data = df['overall']
 
-X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.5)
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.3)
 
 print('Training class distributions summary: {}'.format(Counter(y_train)))
 print('Test class distributions summary: {}'.format(Counter(y_test)))
 
-pipe = make_pipeline(TfidfVectorizer(), MultinomialNB())
+pipe = make_pipeline_imb(TfidfVectorizer(), RandomUnderSampler(), MultinomialNB())
+# pipe = make_pipeline(CountVectorizer(), MultinomialNB())
 pipe.fit(X_train, y_train)
 y_pred = pipe.predict(X_test)
 
 print(classification_report_imbalanced(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+print(confusion_matrix(y_test,y_pred))
