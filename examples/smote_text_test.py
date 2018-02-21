683398f54ba5ec4877e67064e8a359eb813e16e6
Lia <blue.star95@outlook.com>
2018-02-21 15:10:39 +0700

    fix some lines of smote code

--- examples/smote_text_test.py
+++ examples/smote_text_test.py
@@ -14,7 +14,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 from sklearn.model_selection import cross_val_score, cross_val_predict, train_test_split
 
-input_file = '/home/lia/Documents/the_project/dataset/to_use/helpfulness/samples/30percent/7.csv'
+# input_file = '/home/lia/Documents/the_project/dataset/to_use/helpfulness/samples/30percent/1.csv'
+input_file = '/home/lia/Documents/the_project/dataset/output/clean_df.csv'
 df = pd.read_csv(input_file)
 
 X_data = df['reviewText']
@@ -26,20 +27,22 @@ X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.
 vectorizer = CountVectorizer()
 x_train_counts = vectorizer.fit_transform(X_train)
 x_train_counts.toarray()
-tf_transformer = TfidfTransformer().fit(x_train_counts)
-X_train_tf = tf_transformer.transform(x_train_counts)
-X_train_tf.toarray()
+# tf_transformer = TfidfTransformer().fit(x_train_counts)
+# X_train_tf = tf_transformer.transform(x_train_counts)
+X_train_tf = TfidfTransformer().fit_transform(x_train_counts)
+# X_train_tf.toarray()
 # tfidf_transformer = TfidfTransformer()
 
 # sure this can be made more efficient? wth is this
-tf_transformer = TfidfTransformer().fit(x_train_counts)
-X_train_tf = tf_transformer.transform(x_train_counts)
-X_train_tf.toarray()
+# tf_transformer = TfidfTransformer().fit(x_train_counts)
+# X_train_tf = tf_transformer.transform(x_train_counts)
+# X_train_tf = TfidfTransformer().fit_transform(x_train_counts)
+# X_train_tf.toarray()
 
 sm = SMOTE()
-X_res, y_res = sm.fit_sample(X_train_tf, y_train)
+X_res, y_res = sm.fit_sample(x_train_counts, y_train)
 X_res, y_res=sm.fit_sample(X_res,y_res)
-print ('Original data {}'.format (Counter(y_train)))
+print('Original data {}'.format (Counter(y_train)))
 print('Resampled data {}'.format(Counter(y_res)))
 
 # VECTORIZE X_test
@@ -49,6 +52,8 @@ X_test = vectorizer.transform(X_test)
 clf = MultinomialNB()
 # clf = linear_model.LogisticRegression()
 # clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(3,7))
+
+# TRAIN THE CLASSIFIER WITH AVAILABLE TRAINING DATA
 clf.fit(X_res, y_res)
 
 y_pred_class = clf.predict(X_test)
