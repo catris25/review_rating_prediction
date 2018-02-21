8186ac81f4ac44e4852ca35c93c0518500fe918d
Lia <blue.star95@outlook.com>
2018-02-21 17:48:37 +0700

    smote finishing

--- examples/smote_text_test.py
+++ examples/smote_text_test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn import datasets
 from imblearn.datasets import make_imbalance
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTE, RandomOverSampler
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neural_network import MLPClassifier
 from sklearn import linear_model
@@ -14,51 +14,57 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 from sklearn.model_selection import cross_val_score, cross_val_predict, train_test_split
 
-# input_file = '/home/lia/Documents/the_project/dataset/to_use/helpfulness/samples/30percent/1.csv'
+print("Oversampling")
+
+# input_file = '/home/lia/Documents/the_project/dataset/to_use/helpfulness/samples/30percent/6.csv'
 input_file = '/home/lia/Documents/the_project/dataset/output/clean_df.csv'
 df = pd.read_csv(input_file)
 
 X_data = df['reviewText']
 y_data = df['overall']
 
-X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.3)
+# SPLIT INTO TRAINING AND TESTING
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2)
 
-# count_vector= CountVectorizer(ngram_range=(1,1))
+# VECTORIZE AND FIT_TRANSFORM THE TRAINING DATA
 vectorizer = CountVectorizer()
-x_train_counts = vectorizer.fit_transform(X_train)
-x_train_counts.toarray()
-# tf_transformer = TfidfTransformer().fit(x_train_counts)
-# X_train_tf = tf_transformer.transform(x_train_counts)
-X_train_tf = TfidfTransformer().fit_transform(x_train_counts)
-# X_train_tf.toarray()
-# tfidf_transformer = TfidfTransformer()
+# vectorizer = TfidfVectorizer()
+X_train_vectorized = vectorizer.fit_transform(X_train)
 
-# sure this can be made more efficient? wth is this
-# tf_transformer = TfidfTransformer().fit(x_train_counts)
-# X_train_tf = tf_transformer.transform(x_train_counts)
-# X_train_tf = TfidfTransformer().fit_transform(x_train_counts)
-# X_train_tf.toarray()
+# OVERSAMPLE WITH SMOTE
+sm = SMOTE(ratio={2:700},random_state=42)
+# sm = SMOTE(ratio='minority')
+X_res, y_res = sm.fit_sample(X_train_vectorized, y_train)
 
-sm = SMOTE()
-X_res, y_res = sm.fit_sample(x_train_counts, y_train)
-X_res, y_res=sm.fit_sample(X_res,y_res)
+sm = SMOTE(ratio={3:700},random_state=42)
+X_res, y_res = sm.fit_sample(X_res, y_res)
+
+sm = SMOTE(ratio={4:700},random_state=42)
+X_res, y_res = sm.fit_sample(X_res, y_res)
+
+sm = SMOTE(ratio={1:700},random_state=42)
+X_res, y_res = sm.fit_sample(X_res, y_res)
+
+
+# OVERSAMPLE WITH NAIVE RANDOM OVERSAMPLE
+# ros = RandomOverSampler(random_state=0)
+# X_res, y_res = ros.fit_sample(X_train_vectorized, y_train)
+
 print('Original data {}'.format (Counter(y_train)))
 print('Resampled data {}'.format(Counter(y_res)))
 
 # VECTORIZE X_test
-X_test = vectorizer.transform(X_test)
+X_test_vectorized = vectorizer.transform(X_test)
 
 # FIT INTO CLASSIFIER
 clf = MultinomialNB()
-# clf = linear_model.LogisticRegression()
-# clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(3,7))
 
 # TRAIN THE CLASSIFIER WITH AVAILABLE TRAINING DATA
 clf.fit(X_res, y_res)
 
-y_pred_class = clf.predict(X_test)
+y_pred_class = clf.predict(X_test_vectorized)
 
-accu = metrics.accuracy_score(y_pred_class, y_test)
+accu = metrics.accuracy_score(y_test, y_pred_class)
 print(accu)
 
 conf_matrix = metrics.confusion_matrix(y_test,y_pred_class)
