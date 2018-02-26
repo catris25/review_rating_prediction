1e13ece82fca5030fea72267b0457940ed03dc0f
Lia <blue.star95@outlook.com>
2018-02-26 09:58:17 +0700

    smote done for now

--- nb_classification_step.py
+++ nb_classification_step.py
@@ -12,29 +12,7 @@ from collections import Counter
 
 import pandas as pd
 
-def classify_nb(train_df, test_df):
-    # READ TRAINING DATA AND SEPARATE INTO X AND y
-    X_train = train_df['reviewText']
-    y_train = train_df['overall']
-
-    # READ TESTING DATA AND SEPARATE INTO X AND y
-    X_test = test_df['reviewText']
-    y_test = test_df['overall']
-
-    # VECTORIZE AND FIT_TRANSFORM THE TRAINING DATA
-    vectorizer = CountVectorizer()
-    X_train_vectorized = vectorizer.fit_transform(X_train)
-    X_test_vectorized = vectorizer.transform(X_test)
-
-    # OVERSAMPLE WITH SMOTE
-    sm = SMOTE(ratio='all')
-    X_res, y_res = sm.fit_sample(X_train_vectorized, y_train)
-
-    print('Original data {}'.format (Counter(y_train)))
-    print('Resampled data {}'.format(Counter(y_res)))
-
-    X_train_vectorized, y_train = X_res, y_res
-
+def classify_report(X_train_vectorized, y_train, X_test_vectorized, y_test):
     # FIT INTO CLASSIFIER
     clf = MultinomialNB()
 
@@ -53,9 +31,7 @@ def classify_nb(train_df, test_df):
     print(report_matrix)
 
 
-def resample():
-    # DO THE SMOTE THING
-
+def classify_nb(train_df, test_df):
     # READ TRAINING DATA AND SEPARATE INTO X AND y
     X_train = train_df['reviewText']
     y_train = train_df['overall']
@@ -66,12 +42,21 @@ def resample():
 
     # VECTORIZE AND FIT_TRANSFORM THE TRAINING DATA
     vectorizer = CountVectorizer()
-    # vectorizer = TfidfVectorizer()
     X_train_vectorized = vectorizer.fit_transform(X_train)
+    X_test_vectorized = vectorizer.transform(X_test)
+
+    print('Original data {}'.format (Counter(y_train)))
+    classify_report(X_train_vectorized, y_train, X_test_vectorized, y_test)
 
     # OVERSAMPLE WITH SMOTE
     sm = SMOTE(ratio='all')
     X_res, y_res = sm.fit_sample(X_train_vectorized, y_train)
+
+    print('Original data {}'.format (Counter(y_train)))
+    print('Resampled data {}'.format(Counter(y_res)))
+
+    X_train_vectorized, y_train = X_res, y_res
+    classify_report(X_train_vectorized, y_train, X_test_vectorized, y_test)
 
 
 def main():
@@ -85,7 +70,7 @@ def main():
     prep_df = pd.read_csv(input_file)
 
     # SPLIT INTO TRAINING AND TESTING
-    train_df, test_df = train_test_split(prep_df, test_size=0.2)
+    train_df, test_df = train_test_split(prep_df, test_size=0.3)
 
     # USE prep_df AS TRAINING DATA AGAINST THE TESTING DATA
     classify_nb(train_df, test_df)
