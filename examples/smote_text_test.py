c0d9083384e3b750b4879a2428d8c1eef0838bc4
Lia <blue.star95@outlook.com>
2018-03-13 01:23:12 +0700

    trying out feature selection

--- examples/smote_text_test.py
+++ examples/smote_text_test.py
@@ -5,8 +5,8 @@ from imblearn.datasets import make_imbalance
 from imblearn.over_sampling import SMOTE, RandomOverSampler
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neural_network import MLPClassifier
-from sklearn import linear_model
-from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import SelectKBest, chi2, VarianceThreshold
 from collections import Counter
 
 from sklearn import metrics
@@ -30,8 +30,8 @@ def ratio_dict(old_ratio):
 print("Oversampling")
 
 # input_file = '/home/lia/Documents/the_project/dataset/to_use/helpfulness/samples/30percent/6.csv'
-# input_file = '/home/lia/Documents/the_project/dataset/output/clean_df.csv'
-input_file = '/home/lia/Documents/the_project/dataset/top_10_movies/top_10_clean.csv'
+input_file = '/home/lia/Documents/the_project/dataset/output/temp.csv'
+# input_file = '/home/lia/Documents/the_project/dataset/top_10_movies/top_10_clean.csv'
 
 df = pd.read_csv(input_file)
 
@@ -51,6 +51,9 @@ print(X_train.head(10))
 vectorizer = TfidfVectorizer()
 X_train_vectorized = vectorizer.fit_transform(X_train)
 
+# VECTORIZE X_test
+X_test_vectorized = vectorizer.transform(X_test)
+
 # OVERSAMPLE WITH SMOTE
 # sm = SMOTE(ratio={2:700},random_state=42)
 sm = SMOTE(ratio=ratio_dict(Counter(y_train)))
@@ -61,20 +64,22 @@ print('Resampled data {}'.format(Counter(y_res)))
 
 X_train_vectorized, y_train = X_res, y_res
 
-# FEATURE SELECTION WITH chi2
-chi = SelectKBest(chi2)
-X_new = chi.fit_transform(X_train_vectorized, y_train)
+# FEATURE SELECTION
+# selector = VarianceThreshold(threshold=(.8 * (1 - .8)))
+# X_new = selector.fit_transform(X_train_vectorized)
+selector = SelectKBest(chi2, 6500)
+X_new = selector.fit_transform(X_train_vectorized, y_train)
+
 print(X_train_vectorized.shape)
 print(X_new.shape)
 
 X_train_vectorized = X_new
+X_test_vectorized = selector.transform(X_test_vectorized)
 
-# VECTORIZE X_test
-X_test_vectorized = vectorizer.transform(X_test)
 
 # FIT INTO CLASSIFIER
 # clf = MultinomialNB()
-clf = linear_model.LogisticRegression()
+clf = LogisticRegression()
 
 # TRAIN THE CLASSIFIER WITH AVAILABLE TRAINING DATA
 clf.fit(X_train_vectorized, y_train)
