9ee9cf5f4e86fafdf25ef5fdf33dad5bc0d226d5
Lia <blue.star95@outlook.com>
2018-04-29 23:40:17 +0700

    t test but i am not sure

--- calculate_rating_difference.py
+++ calculate_rating_difference.py
@@ -1,12 +1,31 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as st
+
+def calculate_t_score(pop_list, samp_list):
+    mean1 = np.mean(samp_list)
+    mean2 = np.mean(pop_list)
+
+    var1 = np.var(samp_list)
+    var2 = np.var(pop_list)
+
+    standard_error = np.sqrt(var1/len(samp_list)+var2/len(pop_list))
+    t_score = (mean1-mean2)/standard_error
+    prob = st.t.sf(np.abs(t_score),4)
+
+    print("t :",t_score)
+    print("p :",prob)
+    return(t_score, prob)
 
 def calculate_rating(df):
     i_length = len(df['iteration'].value_counts())
 
     for i in range(0, i_length):
+        print("ITERATION-%d"%i)
+
         current_matrix = df[df['iteration'] == i]
         current_matrix = current_matrix.drop('iteration', 1)
+        current_matrix.index = np.arange(1, len(current_matrix) + 1)
         # print(current_matrix)
 
         # GET ACTUAL SCORE BY LOOPING THROUGH THE ROWS
@@ -31,25 +50,11 @@ def calculate_rating(df):
 
         sum_prediction = sum(prediction_list)
 
-        # print("ITERATION-%d"%i)
-
-        # CALCULATE THE DIFFERENCE
-        # print("actual score:",sum_actual)
-        # print("prediction score:",sum_prediction)
-        # diff = abs(sum_actual - sum_prediction)
-        # print(diff)
-
-        if sum_actual > sum_prediction:
-            print("ACTUAL SCORE")
-        elif sum_prediction > sum_actual:
-            print("PREDICTION SCORE")
-        else:
-            print("EQUAL")
+        t_score, prob = calculate_t_score(prediction_list, actual_list)
 
 
 def main():
-    file_folder = '/home/lia/Documents/the_project/output/main_result/'
-    input_file = '/home/lia/Documents/the_project/output/main_result/p_smote_logreg.csv'
+    input_file = '/home/lia/Documents/the_project/output/main_result/sum.csv'
     nb_df = pd.read_csv(input_file, index_col=0)
 
     calculate_rating(nb_df)
