14751501a206d5f70e24c29aaaf627c411da80b4
Lia <blue.star95@outlook.com>
2018-05-04 18:36:44 +0700

    t test

--- calculate_rating_difference.py
+++ calculate_rating_difference.py
@@ -1,20 +1,38 @@
 import pandas as pd
 import numpy as np
 import scipy.stats as st
+from scipy.stats import ttest_ind_from_stats
 
 def calculate_t_score(pop_list, samp_list):
+    n1 = len(samp_list)
+    n2 = len(pop_list)
+
     mean1 = np.mean(samp_list)
     mean2 = np.mean(pop_list)
 
-    var1 = np.var(samp_list)
-    var2 = np.var(pop_list)
+    var1 = np.var(samp_list,ddof=1)
+    # var1 = sum([(xi - mean1) ** 2 for xi in samp_list]) / (n1 - 1)
+    var2 = np.var(pop_list, ddof=1)
 
-    standard_error = np.sqrt(var1/len(samp_list)+var2/len(pop_list))
-    t_score = (mean1-mean2)/standard_error
-    prob = st.t.sf(np.abs(t_score),4)
+    # print("VAR1",var1)
+    # print("VAR2",var2)
+
+    std1 = np.std(samp_list, ddof=1)
+    std2 = np.std(pop_list, ddof=1)
+
+    standard_error = np.sqrt(var1/n1 + var2/n2)
+    # standard_error = std1/np.sqrt(n1) + std2/np.sqrt(n2)
+    t_score = (mean1 - mean2)/standard_error
+    t_score = t_score * 2
+
+    prob = st.t.sf(np.abs(t_score), df=4)
 
     print("t :",t_score)
     print("p :",prob)
+
+    # tstat, pvalue = ttest_ind_from_stats(mean1, std1, n1, mean2, std2, n2)
+    # print(pvalue)
+
     return(t_score, prob)
 
 def calculate_rating(df):
@@ -26,7 +44,7 @@ def calculate_rating(df):
         current_matrix = df[df['iteration'] == i]
         current_matrix = current_matrix.drop('iteration', 1)
         current_matrix.index = np.arange(1, len(current_matrix) + 1)
-        # print(current_matrix)
+        print(current_matrix)
 
         # GET ACTUAL SCORE BY LOOPING THROUGH THE ROWS
         actual_list = []
@@ -58,10 +76,6 @@ def main():
     nb_df = pd.read_csv(input_file, index_col=0)
 
     calculate_rating(nb_df)
-
-    # NOTE TO SELF
-    # CALCULATE THE Z TEST
-    # THERE IS SOMETHING WRONG WITH SMOTE LOGREG PLS CHECK
 
 if __name__ == "__main__":
     main()
