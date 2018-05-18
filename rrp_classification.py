da54b3ae1d7b8c2e680bc6498d33f73c63f2d631
Lia <blue.star95@outlook.com>
2018-05-18 07:42:04 +0700

    encountered an error

--- rrp_classification.py
+++ rrp_classification.py
@@ -192,16 +192,18 @@ def classify_data(df, n_loop):
         print('Proportional \t{}'.format(Counter(y_res_p)))
 
         # APPEND ALL y_pred_class LISTS WITH THEIR CORRESPONDING review_id
-        nb1_y_list.extend([list(x) for x in zip(test_df['review_id'], nb_y)])
-        logreg1_y_list.extend([list(x) for x in zip(test_df['review_id'], logreg_y)])
+        iteration = [i]*len(test_df)
+        nb1_y_list.append([list(x) for x in zip(test_df['review_id'], nb_y, iteration)])
+        logreg1_y_list.append([list(x) for x in zip(test_df['review_id'], logreg_y, iteration)])
 
-        nb2_y_list.extend([list(x) for x in zip(test_df['review_id'], unp_nb_y)])
-        logreg2_y_list.extend([list(x) for x in zip(test_df['review_id'], unp_logreg_y)])
+        nb2_y_list.append([list(x) for x in zip(test_df['review_id'], unp_nb_y, iteration)])
+        logreg2_y_list.append([list(x) for x in zip(test_df['review_id'], unp_logreg_y, iteration)])
 
-        nb3_y_list.extend([list(x) for x in zip(test_df['review_id'], p_nb_y)])
-        logreg3_y_list.extend([list(x) for x in zip(test_df['review_id'], p_logreg_y)])
+        nb3_y_list.append([list(x) for x in zip(test_df['review_id'], p_nb_y, iteration)])
+        logreg3_y_list.append([list(x) for x in zip(test_df['review_id'], p_logreg_y, iteration)])
 
         # END OF LOOP
+
 
     # SAVE ALL RESULTS TO FILES
     output_file = "/home/lia/Documents/the_project/output/"+time.strftime("%Y-%m-%d")+"/"
@@ -219,18 +221,19 @@ def classify_data(df, n_loop):
     logreg3_df = pd.concat(logreg3_list)
 
     # SAVE ALL THE CONCATENATED DATAFRAMES TO THEIR OWN CSV FILES
-    folder_name = output_file+"matrices/"
-    nb1_df.to_csv(folder_name+"nb.csv")
-    nb2_df.to_csv(folder_name+"unp_smote_nb.csv")
-    nb3_df.to_csv(folder_name+"p_smote_nb.csv")
-    logreg1_df.to_csv(folder_name+"logreg.csv")
-    logreg2_df.to_csv(folder_name+"unp_smote_logreg.csv")
-    logreg3_df.to_csv(folder_name+"p_smote_logreg.csv")
-    print("confusion matrices all saved")
+    # folder_name = output_file+"matrices/"
+    # nb1_df.to_csv(folder_name+"nb.csv")
+    # nb2_df.to_csv(folder_name+"unp_smote_nb.csv")
+    # nb3_df.to_csv(folder_name+"p_smote_nb.csv")
+    # logreg1_df.to_csv(folder_name+"logreg.csv")
+    # logreg2_df.to_csv(folder_name+"unp_smote_logreg.csv")
+    # logreg3_df.to_csv(folder_name+"p_smote_logreg.csv")
+    # print("confusion matrices all saved")
 
     # SAVE REVIEW DATA WITH THEIR PREDICTED CLASSES
     folder_name = output_file +"data/"
-    pd.DataFrame(nb1_y_list).to_csv(folder_name+"nb.csv")
+    print(pd.DataFrame(nb1_y_list))
+    pd.DataFrame(nb1_y_list, columns=['review_id', 'prediction', 'iteration'], index=False).to_csv(folder_name+"nb.csv")
     pd.DataFrame(nb2_y_list).to_csv(folder_name+"unp_smote_nb.csv")
     pd.DataFrame(nb3_y_list).to_csv(folder_name+"p_smote_nb.csv")
     pd.DataFrame(logreg1_y_list).to_csv(folder_name+"logreg.csv")
@@ -251,7 +254,7 @@ def main():
     print(" %d reviews of %d movies"%(n_reviews, n_movies))
     print(prep_df['overall'].value_counts().sort_index())
 
-    n_loop = 5
+    n_loop = 3
     classify_data(prep_df, n_loop)
 
 
