fc0577d4ecc6b566b7b017ebe6dc13dfeee39c4c
Lia <blue.star95@outlook.com>
2018-06-26 22:29:59 +0700

    added saving sum of matrices and others

--- rrp_classification.py
+++ rrp_classification.py
@@ -18,6 +18,18 @@ import numpy as np
 
 import os, time
 
+# SUM ALL MATRICES
+def sum_all_matrices(matrix_list, m_name):
+    sum_matrix = np.sum([df.iloc[:,0:5].values for df in matrix_list], axis=0)
+    print(sum_matrix)
+    avg_accu = sum(np.diag(sum_matrix))/sum_matrix.sum()
+    print(avg_accu)
+
+    matrix_df = pd.DataFrame(sum_matrix, columns=[1,2,3,4,5])
+    matrix_df['name'] = m_name
+    matrix_df.index = np.arange(1, len(matrix_df) + 1)
+    return matrix_df
+
 # GENERATE A DICTIONARY THAT WILL DECIDE HOW MANY RESAMPLES FOR EACH CLASS SHOULD GET
 # USED IN PROPORTIONAL OVERSAMPLING
 def ratio_dict(old_ratio):
@@ -193,22 +205,38 @@ def classify_data(df, n_loop):
 
         # APPEND ALL y_pred_class LISTS WITH THEIR CORRESPONDING review_id
         iteration = [i]*len(test_df)
-        nb1_y_list.extend([list(x) for x in zip(test_df['review_id'], nb_y, iteration)])
-        logreg1_y_list.extend([list(x) for x in zip(test_df['review_id'], logreg_y, iteration)])
+        nb1_y_list.extend([list(x) for x in zip(test_df['review_id'], y_test, nb_y, iteration)])
+        logreg1_y_list.extend([list(x) for x in zip(test_df['review_id'], y_test, logreg_y, iteration)])
 
-        nb2_y_list.extend([list(x) for x in zip(test_df['review_id'], unp_nb_y, iteration)])
-        logreg2_y_list.extend([list(x) for x in zip(test_df['review_id'], unp_logreg_y, iteration)])
+        nb2_y_list.extend([list(x) for x in zip(test_df['review_id'], y_test, unp_nb_y, iteration)])
+        logreg2_y_list.extend([list(x) for x in zip(test_df['review_id'], y_test, unp_logreg_y, iteration)])
 
-        nb3_y_list.extend([list(x) for x in zip(test_df['review_id'], p_nb_y, iteration)])
-        logreg3_y_list.extend([list(x) for x in zip(test_df['review_id'], p_logreg_y, iteration)])
+        nb3_y_list.extend([list(x) for x in zip(test_df['review_id'], y_test, p_nb_y, iteration)])
+        logreg3_y_list.extend([list(x) for x in zip(test_df['review_id'], y_test, p_logreg_y, iteration)])
 
         # END OF LOOP
 
+    # SUM ALL THE MATRICES TO GET THE AVERAGE ACCURACY FROM MULTIPLE ITERATIONS
+    # MNB RESULTS
+    print("MultinomialNB, Unproportional SMOTE + MNB, Proportional SMOTE + MNB")
+    nb1_sum = sum_all_matrices(nb1_list, "nb")
+    nb2_sum = sum_all_matrices(nb2_list, "unp_smote_nb")
+    nb3_sum = sum_all_matrices(nb3_list, "p_smote_nb")
+    # LOGREG RESULTS
+    print("Logreg, Unproportional SMOTE + Logreg, Proportional SMOTE + Logreg")
+    logreg1_sum = sum_all_matrices(logreg1_list, "logreg")
+    logreg2_sum = sum_all_matrices(logreg2_list, "unp_smote_logreg")
+    logreg3_sum = sum_all_matrices(logreg3_list, "p_smote_logreg")
+
     # SAVE ALL RESULTS TO FILES
-    output_file = "/home/lia/Documents/the_project/output/"+time.strftime("%Y-%m-%d")+"/"
+    output_file = "/home/lia/Dropbox/output/"+time.strftime("%Y-%m-%d")+"/"
     os.makedirs("%s"%output_file, exist_ok=True)
     os.makedirs("%smatrices"%output_file, exist_ok=True)
     os.makedirs("%sdata"%output_file, exist_ok=True)
+
+    # SAVE THE SUM RESULTS
+    sum_df = pd.concat([nb1_sum, nb2_sum, nb3_sum, logreg1_sum, logreg2_sum, logreg3_sum])
+    sum_df.to_csv(output_file+"sum.csv")
 
     # SAVE CONFUSION MATRICES OF ALL CLASSIFICATIONS
     # CONCAT ALL THE DATAFRAMES INSIDE LISTS
@@ -219,29 +247,29 @@ def classify_data(df, n_loop):
     logreg2_df = pd.concat(logreg2_list)
     logreg3_df = pd.concat(logreg3_list)
 
-    # # SAVE ALL THE CONCATENATED DATAFRAMES TO THEIR OWN CSV FILES
-    # folder_name = output_file+"matrices/"
-    # nb1_df.to_csv(folder_name+"nb.csv")
-    # nb2_df.to_csv(folder_name+"unp_smote_nb.csv")
-    # nb3_df.to_csv(folder_name+"p_smote_nb.csv")
-    # logreg1_df.to_csv(folder_name+"logreg.csv")
-    # logreg2_df.to_csv(folder_name+"unp_smote_logreg.csv")
-    # logreg3_df.to_csv(folder_name+"p_smote_logreg.csv")
-    # print("confusion matrices all saved")
-    #
-    # # SAVE REVIEW DATA WITH THEIR PREDICTED CLASSES
-    # folder_name = output_file +"data/"
-    # col_list = ['review_id', 'prediction', 'iteration']
-    # pd.DataFrame(nb1_y_list, columns=col_list).to_csv(folder_name+"nb.csv", index=False)
-    # pd.DataFrame(nb2_y_list, columns=col_list).to_csv(folder_name+"unp_smote_nb.csv", index=False)
-    # pd.DataFrame(nb3_y_list, columns=col_list).to_csv(folder_name+"p_smote_nb.csv", index=False)
-    # pd.DataFrame(logreg1_y_list, columns=col_list).to_csv(folder_name+"logreg.csv", index=False)
-    # pd.DataFrame(logreg2_y_list, columns=col_list).to_csv(folder_name+"unp_smote_logreg.csv", index=False)
-    # pd.DataFrame(logreg3_y_list, columns=col_list).to_csv(folder_name+"p_smote_logreg.csv", index=False)
-    # print("result dataframes all saved")
-    #
-    # # SAVE ORIGINAL DATAFRAME FOR LATER REFERENCE
-    # df.to_csv(output_file+"df.csv", index=False)
+    # SAVE ALL THE CONCATENATED DATAFRAMES TO THEIR OWN CSV FILES
+    folder_name = output_file+"matrices/"
+    nb1_df.to_csv(folder_name+"nb.csv")
+    nb2_df.to_csv(folder_name+"unp_smote_nb.csv")
+    nb3_df.to_csv(folder_name+"p_smote_nb.csv")
+    logreg1_df.to_csv(folder_name+"logreg.csv")
+    logreg2_df.to_csv(folder_name+"unp_smote_logreg.csv")
+    logreg3_df.to_csv(folder_name+"p_smote_logreg.csv")
+    print("confusion matrices all saved")
+
+    # SAVE REVIEW DATA WITH THEIR PREDICTED CLASSES
+    folder_name = output_file +"data/"
+    col_list = ['review_id', 'actual', 'prediction', 'iteration']
+    pd.DataFrame(nb1_y_list, columns=col_list).to_csv(folder_name+"nb.csv", index=False)
+    pd.DataFrame(nb2_y_list, columns=col_list).to_csv(folder_name+"unp_smote_nb.csv", index=False)
+    pd.DataFrame(nb3_y_list, columns=col_list).to_csv(folder_name+"p_smote_nb.csv", index=False)
+    pd.DataFrame(logreg1_y_list, columns=col_list).to_csv(folder_name+"logreg.csv", index=False)
+    pd.DataFrame(logreg2_y_list, columns=col_list).to_csv(folder_name+"unp_smote_logreg.csv", index=False)
+    pd.DataFrame(logreg3_y_list, columns=col_list).to_csv(folder_name+"p_smote_logreg.csv", index=False)
+    print("result dataframes all saved")
+
+    # SAVE ORIGINAL DATAFRAME FOR LATER REFERENCE
+    df.to_csv(output_file+"df.csv", index=False)
 
 
 def main():
@@ -256,7 +284,7 @@ def main():
     print(" %d reviews of %d movies"%(n_reviews, n_movies))
     print(prep_df['overall'].value_counts().sort_index())
 
-    n_loop = 1
+    n_loop = 30
     classify_data(prep_df, n_loop)
 
 
